48af1bb5944de9bfbc2789e67898d229b29fcf70
apsteinmetz <apsteinmetz@yahoo.com>
2017-08-17 16:34:29 -0400

    added tabs to ui

--- WFMU_Explorer/ui.R
+++ WFMU_Explorer/ui.R
@@ -1,34 +1,55 @@
 
 library(shiny)
 
-shinyUI(fluidPage(
-  # Application title
-  titlePanel("Top Artists and Songs Played on WFMU"),
-  
-  sidebarLayout(
-    # Sidebar with a slider and selection inputs
-    sidebarPanel(
-      selectInput("selection", "On Current Schedule:",
-                  choices = c('YES','NO','ALL')),
-      hr(),
-      sliderInput("years_back",
-                  "Years Back to Include:",
-                  min = 0,  max = 30, value = c(0,5)),
-      actionButton("update","Update")
-    ),
-    
-    # Show Word Cloud
-    mainPanel(
-      fluidRow(
-        h4('Top Artists'),
-        tabsetPanel(type = "tabs",
-                  tabPanel("Word Cloud", plotOutput("cloud")),
-                  tabPanel("Table", tableOutput("table_artists"))
-      )),
-      fluidRow(
-        h4('Top Songs'),
-        tableOutput("table_songs")
-      )
-    )
-  )
+shinyUI(navbarPage("WFMU Explorer",
+                   # Application title
+                   # tabPanel("Start Here",
+                   #          mainPanel(
+                   #            h4("Start by loading playlists.  This takes a minute but only needs to be done once."),
+                   #            actionButton("load","Load Playlists")
+                   #          )
+                   # ),
+                   tabPanel("Station",
+                            titlePanel("Top Artists and Songs Played on WFMU"),
+                            
+                            sidebarLayout(
+                              # Sidebar with a slider and selection inputs
+                              sidebarPanel(
+                                selectInput("selection", "On Current Schedule:",
+                                            choices = c('YES','NO','ALL')),
+                                hr(),
+                                sliderInput("years_back",
+                                            "Years Back to Include:",
+                                            min = 0,  max = 30, value = c(0,5)),
+                                actionButton("update","Update")
+                              ),
+                              
+                              # Show Word Cloud
+                              mainPanel(
+                                fluidRow(
+                                  h4('Top Artists'),
+                                  tabsetPanel(type = "tabs",
+                                              tabPanel("Word Cloud", plotOutput("cloud")),
+                                              tabPanel("Table", tableOutput("table_artists"))
+                                  )),
+                                fluidRow(
+                                  h4('Top Songs'),
+                                  tableOutput("table_songs")
+                                )
+                              )
+                            )
+                   ),
+                   tabPanel("DJs",
+                            titlePanel("Nothing Here Yet")
+                   ),
+                   tabPanel("Artists",
+                            titlePanel("Nothing Here Yet")
+                   ),
+                   tabPanel("Songs",
+                            titlePanel("Nothing Here Yet")
+                   ),
+                   tabPanel("About",
+                            titlePanel("Nothing Here Yet")
+                   )
+                   
 ))
