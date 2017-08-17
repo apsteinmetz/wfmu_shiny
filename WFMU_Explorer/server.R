e8977c0cbebb00f905471d12fff72a94bbb6b523
apsteinmetz <apsteinmetz@yahoo.com>
2017-08-16 23:01:15 -0400

    working

--- WFMU_Explorer/server.R
+++ WFMU_Explorer/server.R
@@ -10,24 +10,53 @@
 library(shiny)
 library(memoise)
 library(wordcloud)
+library(dplyr)
 
-#load("./playlists.rdata")
-#load('./djkey.rdata')
+load("./playlists.rdata")
+load('./djkey.rdata')
 DJKey$DJ<-as.character(DJKey$DJ)
-#DJKey<-DJKey %>% 
-#  mutate(onMic=ifelse(onSched,'YES','NO')) %>% select(DJ,ShowName,onSched=onMic,showCount)
+playlists<-playlists %>% 
+  ungroup() %>% 
+  mutate(artist_song=paste(ArtistToken,Title))
 
 get_top_artists<-memoise(function(onAir,years_back) {
-  top_artists<-DJKey %>% 
-    filter(onSched==onAir) %>% #on Sched or off?
-    select(DJ) %>% 
+  if (onAir=='ALL') {
+    DJ_set <-DJKey %>% 
+      select(DJ)
+  } else {
+    DJ_set <-DJKey %>% 
+      filter(onSched==onAir) %>% #on Sched or off?
+      select(DJ) 
+      
+  }
+  top_artists<-DJ_set %>% 
     left_join(playlists,by='DJ') %>%
-    filter(AirDate>Sys.Date()-(365*years_back)) %>%  #date range?
+    filter(AirDate>Sys.Date()-(365*years_back[2])) %>%  #date range?
+    filter(AirDate<Sys.Date()-(365*years_back[1])) %>%  #date range?
     group_by(ArtistToken)%>%
     summarize(play_count=n())%>%
     top_n(100) %>% 
     arrange(desc(play_count))
   top_artists
+})
+
+get_top_songs<-memoise(function(onAir,years_back) {
+  if (onAir=='ALL') {
+    DJ_set <-playlists
+  } else {
+    DJ_set <-DJKey %>% 
+      filter(onSched==onAir) %>% #on Sched or off?
+      select(DJ) %>%  
+      left_join(playlists,by='DJ')
+  }
+  top_songs<-DJ_set %>% 
+    filter(AirDate>Sys.Date()-(365*years_back[2])) %>%  #date range?
+    filter(AirDate<Sys.Date()-(365*years_back[1])) %>%  #date range?
+    group_by(artist_song)%>%
+    summarize(play_count=n())%>%
+    top_n(25) %>% 
+    arrange(desc(play_count))
+  top_songs
 })
 
 
@@ -44,16 +73,31 @@ shinyServer(function(input, output) {
     })
     return(ret_val)
   })
+  top_songs_reactive<-reactive({
+    input$update
+    isolate({      
+      withProgress({
+        setProgress(message = "Processing...")
+        ret_val<-get_top_songs(input$selection,input$years_back)
+      })
+    })
+    return(ret_val)
+  })
   
   output$cloud <- renderPlot({
     wordcloud_rep <- repeatable(wordcloud,seed=1234)
     top_artists<-top_artists_reactive() 
     scaleFactor=2
     wordcloud_rep(words = top_artists$ArtistToken, 
-                    freq = top_artists$play_count^scaleFactor,
-                    max.words=100, 
-                    random.order=FALSE,rot.per=0.35, 
-                    colors=brewer.pal(8, "Dark2"),
-                    scale = c(4,.3))
-    })
+                  freq = top_artists$play_count^scaleFactor,
+                  max.words=100, 
+                  random.order=FALSE,rot.per=0.35, 
+                  colors=brewer.pal(8, "Dark2"),
+                  scale = c(4,.3))
+  })
+  output$table_artists <- renderTable({
+    top_artists_reactive()
+  })
+  
+
 })
--- WFMU_Explorer/ui.R
+++ WFMU_Explorer/ui.R
@@ -3,23 +3,30 @@ library(shiny)
 
 shinyUI(fluidPage(
   # Application title
-  titlePanel("Top Artists on WFMU"),
+  titlePanel("Top Artists and Songs Played on WFMU"),
   
   sidebarLayout(
     # Sidebar with a slider and selection inputs
     sidebarPanel(
       selectInput("selection", "On Current Schedule:",
-                  choices = c('YES','NO')),
+                  choices = c('YES','NO','ALL')),
       hr(),
       sliderInput("years_back",
                   "Years Back to Include:",
-                  min = 1,  max = 25, value = 5),
+                  min = 0,  max = 25, value = c(0,5)),
       actionButton("update","Update")
     ),
     
     # Show Word Cloud
     mainPanel(
-      plotOutput("cloud")
+      h4('Top Artists'),
+      tabsetPanel(type = "tabs",
+                  tabPanel("Word Cloud", plotOutput("cloud")),
+                  tabPanel("Table", tableOutput("table_artists"))
+      )#,
+      
+      #h4('Top Songs'),
+      #tableOutput("table")
     )
   )
 ))
