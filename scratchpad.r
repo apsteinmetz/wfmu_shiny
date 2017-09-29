687f73cefc830bb3cc401c95b26935cefdc75ea3
apsteinmetz <apsteinmetz@yahoo.com>
2017-09-28 21:55:23 -0400

    scratchpad

--- scratchpad.r
+++ scratchpad.r
@@ -5,18 +5,25 @@ playlists %>%
   mutate(AirDate=year(AirDate)) %>% 
   group_by(AirDate, ArtistToken) %>% 
   summarize(PlayCount=n()) %>% 
-    ggplot(aes(AirDate,PlayCount,fill=ArtistToken))+geom_col()+scale_y_continuous(breaks = 1:20)
+    ggplot(aes(AirDate,PlayCount,fill=ArtistToken))+
+  geom_col()+
+  scale_y_continuous(breaks = seq(0,18,by=2))+
+  theme_economist()
 
 temp<-playlists %>%
   group_by(ArtistToken) %>% 
   summarise(n=n()) %>% 
   arrange(desc(n))
 
-playlists %>%
+gg<-playlists %>%
   filter(ArtistToken %in% c("Stooges","Voivod","NinaSimone")) %>% 
   left_join(DJKey) %>%
   mutate(AirDate=year(AirDate)) %>% 
-  group_by(AirDate,ArtistToken) %>% 
+  filter(AirDate>2005) %>%
+  mutate(AirDate=factor(AirDate)) %>% 
+    group_by(AirDate,ArtistToken) %>% 
   summarize(PlayCount=n()) %>% 
   arrange(desc(PlayCount)) %>% 
   ggplot(aes(AirDate,PlayCount,fill=ArtistToken))+geom_col()
+
+gg
